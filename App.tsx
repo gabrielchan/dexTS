d6e4e8f5523c43485646f4dcb30d4024fae1a0fa
Gabriel Chan <gchan0507@gmail.com>
2020-01-05 09:52:07 -0500

    refactored Card to also contain the FlatList in CardView. CardView deleted as it's current design does not need it to be a stateful component

--- App.tsx
+++ App.tsx
@@ -13,13 +13,13 @@ import {
   View,
   ScrollView,
 } from 'react-native';
-import CardView from './components/class/CardView';
+import CardList from './components/presentation/CardList';
 import testData from './test_data/sampleCardData.json'; //testData is a JSON Array (not a JSON Object)
 
 const App = () => {
   const usingHermes = typeof HermesInternal === 'object' && HermesInternal !== null;
   return (
-    <CardView cardData={testData} />
+    <CardList cardList={testData} />
   );
 };
 
---
+++ components/presentation/CardList.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  FlatList,
+} from 'react-native';
+import {
+  Card,
+  Text,
+} from 'react-native-elements';
+
+export interface CardItems {
+  header: string,
+  content: string,
+  subcontent: string
+};
+
+interface Props {
+  cardList: CardItems[];
+}
+
+const CardList: React.SFC<Props> = (props) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={props.cardList}
+        keyExtractor={(item) => item.header}
+        renderItem={({ item }) =>
+          <Card
+            title={item.header}>
+            <Text style={styles.content}>
+              {item.content}
+            </Text>
+            <Text style={styles.subcontent}>
+              {item.subcontent}
+            </Text>
+          </Card>
+        }
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  subcontent: {
+    marginBottom: 10,
+    fontWeight: "bold"
+  },
+  content: {
+    marginBottom: 10,
+    color: "grey"
+  },
+  container: {
+    flex: 1,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+})
+
+export default CardList;

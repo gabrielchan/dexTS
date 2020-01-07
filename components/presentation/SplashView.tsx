6311d0e7ecf69359abfb51bb8ff5f995ee949e58
Gabriel Chan <gabriel.chan@capco.com>
2020-01-07 16:10:41 -0500

    fixing splashview to exclude bad code

--- components/presentation/SplashView.tsx
+++ components/presentation/SplashView.tsx
@@ -29,10 +29,6 @@ const SplashView: React.FC<Props> = (props) => {
           title="Go to CardList"
           onPress={() => props.navigation.navigate('CardList')}
         />
-        <Button
-          title="Go to SimpleList"
-          onPress={() => props.navigation.navigate('SimpleList')}
-        />
       </View>
     </View>
   );

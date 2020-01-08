0a28e2fe9d3a302ad6c699c01b037d8514c73654
Gabriel Chan <gabriel.chan@capco.com>
2020-01-08 10:11:41 -0500

    abstracting the text view of splash outside of the page as it may change in the future

--- components/presentation/SplashView.tsx
+++ components/presentation/SplashView.tsx
@@ -6,6 +6,7 @@ import {
   Button,
   Alert,
 } from 'react-native';
+import TextView from './TextView';
 
 const navigationOptions = {
   title: 'SplashView',
@@ -19,28 +20,7 @@ interface Props {
 const SplashView: React.FC<Props> = (props) => {
   return (
     <View style={styles.container}>
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>Values</Text>
-        <Text>Empathy, Honesty, Respect, and Curiosity</Text>
-        <Text style={styles.title}>Value-Driven Commandments</Text>
-        <Text>
-          1. [Honesty] Love oneself{'\n'}
-          2. [Curiosity] Enjoy the Journey {'\n'}
-          3. [Respect] Alone time is an opportunity to grow{'\n'}
-          4. [Empathy] Feel ones feelings and address them healthily{'\n'}
-        </Text>
-        <Text style={styles.title}>Why Am I Walking Down this Path?</Text>
-        <Text>
-          After a tough but triumphant 2019 year, I've come to realize that I want to center my life around the four key values.
-          With work, I have decided to <Text style={{fontWeight: 'bold'}}> become a Developer</Text> because I've always had a
-          curiosity to Mathematics and problems. This application represents:{'\n'}
-          1. My <Text style={{fontWeight: 'bold'}}>curiosity</Text> for knowledge{'\n'}
-          2. Being <Text style={{fontWeight: 'bold'}}>honest</Text> to myself about my capabilities in light of other's opinions{'\n'}
-          3. <Text style={{fontWeight: 'bold'}}>Empathsizing</Text> with my desires to look out for my best interest{'\n'}
-          4. <Text style={{fontWeight: 'bold'}}>Respecting</Text> and loving myself
-          5. My <Text style={{fontWeight: 'bold'}}>independence</Text> and learning how to take care of myself
-        </Text>
-      </View>
+      <TextView />
       <View style={styles.buttonContainer}>
         <Button
           title="Test Button"
@@ -61,19 +41,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start'
   },
-  textContainer: {
-    flex: 1,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start'
-  },
   buttonContainer: {
     flex: 1,
     alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'space-around'
-  },
-  title: {
-    fontSize: 30
   },
 });
 

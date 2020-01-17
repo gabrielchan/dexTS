096e06b20113a832da5efd1425f06ff43559511e
Gabriel Chan <gabriel.chan@capco.com>
2020-01-17 11:29:02 -0500

    adding textbox and screen

--- components/screen/VoidScreen.tsx
+++ components/screen/VoidScreen.tsx
@@ -2,21 +2,18 @@ import React, { useState } from 'react';
 import {
   View,
   StyleSheet,
-  FlatList,
   Text,
 } from 'react-native';
 import { NavigationStackScreenComponent } from 'react-navigation-stack';
 import { Input } from 'react-native-elements';
-//import MessageInputBox from '../presentation/MessageInputBox';
+import Message from '../presentation/Messages';
 
 const VoidScreen: NavigationStackScreenComponent = () => {
   const [input, setInput] = useState<string>('');
   const [messages, setMessages] = useState<Array<string>>([]);
   return (
     <View style={styles.container}>
-      <View>
-        <Text>{messages}</Text>
-      </View>
+      <Message user="Gabriel" message={input}/>
       <View style={styles.inputBoxContainer}>
         <Input
           placeholder="Type a message..."

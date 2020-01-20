26fbbacc6857cc01c21e05f9c66b3a2d44910796
Gabriel Chan <gchan0507@gmail.com>
2020-01-20 14:45:36 -0500

    implemented gifted chat, removed messageinputbox

--- components/screen/VoidScreen.tsx
+++ components/screen/VoidScreen.tsx
@@ -2,26 +2,14 @@ import React, { useState } from 'react';
 import {
   View,
   StyleSheet,
-  Text,
 } from 'react-native';
 import { NavigationStackScreenComponent } from 'react-navigation-stack';
-import { Input } from 'react-native-elements';
 import Message from '../presentation/Messages';
 
 const VoidScreen: NavigationStackScreenComponent = () => {
-  const [input, setInput] = useState<string>('');
-  const [messages, setMessages] = useState<Array<string>>([]);
   return (
     <View style={styles.container}>
-      <Message user="Gabriel" message={input}/>
-      <View style={styles.inputBoxContainer}>
-        <Input
-          placeholder="Type a message..."
-          onChangeText={(text: string) => setInput(text)}
-          onSubmitEditing={() => setMessages([...messages, input])}
-          value={input}
-        />
-      </View>
+      <Message />
     </View>
   );
 };
